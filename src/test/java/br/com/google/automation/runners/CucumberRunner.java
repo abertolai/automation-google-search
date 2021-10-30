bc9c7ef8e0d1cf2e31f50df35cf86c29776222fb
amandabertolai <amanda.bertolai@matera.com>
2021-10-30 19:45:00 -0300

    Perform configuration to use chromeDriver

--- src/test/java/br/com/google/automation/runners/CucumberRunner.java
+++ src/test/java/br/com/google/automation/runners/CucumberRunner.java
@@ -1,16 +1,29 @@
 package br.com.google.automation.runners;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import br.com.google.automation.common.Base;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/cucumber.json"},
+        monochrome = true,
         features = "src/test/resources/features"
         ,glue= {"br.com.google.automation"}
 )
 
 public class CucumberRunner {
 
+    @BeforeClass
+    public static void setup() {
+        String browserName = System.getProperty("browser");
+        Base.setDriver(browserName);
+    }
 }
