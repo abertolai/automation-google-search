705fb20484e2d06d01933bd8dd7ea30c4dcc8d6a
amandabertolai <amanda.bertolai@matera.com>
2021-11-02 15:19:25 -0300

    Added code for first scenario and created second scenario

--- src/test/java/br/com/google/automation/common/Base.java
+++ src/test/java/br/com/google/automation/common/Base.java
@@ -1,8 +1,13 @@
 package br.com.google.automation.common;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
     public static WebDriver driver;
@@ -15,7 +20,38 @@ public class Base {
         driver.get(url);
     }
 
-    public static WebElement getElement() {
-        return driver.findElement(By.name("q"));
+    public static WebElement getElement(By locator) {
+        return driver.findElement(locator);
     }
+
+    public static void clickOn(By locator) {
+        isClickable(locator);
+        getElement(locator).click();
+    }
+
+    public static void clickSubmit(By locator) {
+        getElement(locator).submit();
+    }
+
+    public static void fillInput(By locator, String data) {
+        waitElement(locator);
+        getElement(locator);
+        clickOn(locator);
+        getElement(locator).sendKeys(data);
+    }
+
+    public static WebElement waitElement(By locator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public static boolean isClickable(By locator) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(getElement(locator)));
+        return getElement(locator).isDisplayed() && getElement(locator).isEnabled();
+    }
+
+    public static List<WebElement> getElements(By locator) {
+        return driver.findElements(locator);
+    }
+
 }
--- src/test/java/br/com/google/automation/pages/SearchPage.java
+++ src/test/java/br/com/google/automation/pages/SearchPage.java
@@ -1,11 +1,44 @@
 package br.com.google.automation.pages;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import br.com.google.automation.common.Base;
 
 public class SearchPage extends Base {
 
-    private static final By INPUT_SEARCH = By.id("q");
+    private static final By INPUT_SEARCH = By.name("q");
+    private static final By BUTTON_SEARCH = By.name("btnK");
+    private static String URL_GOOGLE = "http://google.com.br";
+    private static By listOfElements = By.cssSelector(".lEBKkf");
+    private static By messageNotFind = By.className("v3jTId");
 
+    public static void searchSomething(String text) {
+        fillInput(INPUT_SEARCH, text);
+        clickSubmit(BUTTON_SEARCH);
+    }
 
+    public static void visitUrlGoogle() {
+        visitUrl(URL_GOOGLE);
+    }
+
+    public static List<WebElement> findElementsContainsText() {
+        return getElements(listOfElements);
+    }
+
+    public static List<String> getTextOfAList() {
+        List<String> listElementsText = new ArrayList<>();
+
+        for (int i=0; findElementsContainsText().size() > i; i++){
+            listElementsText.add(findElementsContainsText().get(i).getText());
+        }
+
+        return listElementsText;
+    }
+
+    public static String getTextMessageNotFind() {
+        return getElement(messageNotFind).getText();
+    }
 }
